3c22da093ea148acb0a656559cb5b5413e490b98
Laura Goyeneche <53354917+lgoyenec@users.noreply.github.com>
2019-09-26 23:48:20 -0400

    Data .xsl and App update
    - Add new tab: analysis of consumption. The Observatory of Drugs in Colombia doesn't count with much information about the consumption of drugs in Colombia. However, I decided to few of it's variables, so that the app can provide a general understanding of the drug market in Colombia.
    - Additional changes in the structure of the app before adding the plots/tab already created.

--- app/app.R
+++ app/app.R
@@ -5,8 +5,8 @@
 
 # Libraries
 library(shiny)
-library(shinydashboard)
 library(shinythemes)
+library(shinydashboard)
 
 library(readxl)
 
@@ -19,6 +19,16 @@ crops       = read_excel("obs_drug_colombia.xlsx", sheet = "illicit_crops")
 eradication = read_excel("obs_drug_colombia.xlsx", sheet = "manual_eradication")
 seizures    = read_excel("obs_drug_colombia.xlsx", sheet = "seizures")
 
+
+convertMenuItem <- function(mi,tabName) {
+  mi$children[[1]]$attribs['data-toggle']="tab"
+  mi$children[[1]]$attribs['data-value'] = tabName
+  if(length(mi$attribs$class)>0 && mi$attribs$class=="treeview"){
+    mi$attribs$class=NULL
+  }
+  mi
+}
+
 # User interface
 # -------------------------------------------------------------------
 ui = 
@@ -28,52 +38,64 @@ ui =
     dashboardSidebar(
       sidebarMenu(
         id = "tabs",
-        menuItem("Supply", 
-                 tabName = "supply", 
-                 icon = icon("seedling"),
-                 selectInput("input1", 
-                             "Variable of interest",
-                             choices  = c("Illicit crops" = "crops", "Manual eradication" = "eradication", "Seizures" = "seizures"), 
-                             selected = "crops"),
-                 sliderInput("slider1", 
-                             "Time period", 
-                             1999, 2018, 2018, 
-                             sep = "")
-                 ),
-        menuItem("Consumption", 
-                 tabName = "demand", 
-                 icon = icon("user-astronaut"),
-                 selectInput("input2",
-                             "Substance",
-                             choices  = c("Marihuana","Cocaine","Heroin"),
-                             selected = "Cocaine"),
-                 radioButtons("button1",
-                              "Year of epidemiological study",
-                              choices  = c(2008, 2011),
-                              selected = 2011)
-                 ),
-        menuItem('Criminality', 
-                 tabName = "criminality", 
-                 icon = icon("skull"),
-                 radioButtons("button2",
-                              "Gender",
-                              choices  = c("Total","Male","Female"),
-                              selected = "Total"),
-                 radioButtons("button3",
-                              "Judicial situation",
-                              choices = c("Convicted or Accused","Convicted", "Accused"),
-                              selected = "All prisoners")
-                 )
+        convertMenuItem(
+          menuItem("Supply", 
+                   tabName = "supply", 
+                   icon = icon("seedling"),
+                   startExpanded = T,
+                   selectInput("input1", 
+                               "Variable of interest",
+                               choices  = c("Illicit crops" = "crops", "Manual eradication" = "eradication", "Seizures" = "seizures"), 
+                               selected = "crops"),
+                   sliderInput("slider1", 
+                               "Time period", 
+                               1999, 2018, value = c(2015,2018), 
+                               sep = "")
+          ), "supply"
+        ),
+        convertMenuItem(
+          menuItem("Consumption", 
+                   tabName = "demand", 
+                   icon = icon("user-astronaut"),
+                   checkboxGroupInput("input2",
+                                      "Substance",
+                                      choices  = c("Marihuana","Cocaine","Heroin"),
+                                      selected = "Cocaine"),
+                   radioButtons("button1",
+                                "Year of epidemiological study",
+                                choices  = c(2008, 2013),
+                                selected = 2013)
+          ), "demand"
+        ),
+        convertMenuItem(
+          menuItem('Criminality', 
+                   tabName = "criminality", 
+                   icon = icon("skull"),
+                   radioButtons("button2",
+                                "Gender",
+                                choices  = c("Total","Male","Female")),
+                   radioButtons("button3",
+                                "Judicial situation",
+                                choices = c("Convicted or Accused","Convicted", "Accused"))
+          ), "criminality"
+        )
       )
     ),
     dashboardBody(
-      
+      tabItems(
+        tabItem(tabName = "supply",p("Laura")),
+        tabItem(tabName = "demand",h2("Y")),
+        tabItem(tabName = "criminality",h2("Z"))
+      )
     )
 )
 
+
+
+
 # Server function
 # -------------------------------------------------------------------
-server <- function(input, output) {
+server = function(input, output) {
 
 }
 
